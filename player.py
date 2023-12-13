b75b601c5baaa7ac223f99486fd029ed8c2dcdc4
Samuel Cabrera <scabrera197@gmail.com>
2023-12-12 23:55:03 -0600

    bounce physics functional

--- player.py
+++ player.py
@@ -61,21 +61,13 @@ class Player:
             return [vel / math.sqrt(2) for vel in self.acceleration]
         return self.acceleration.copy()
 
-    @property
-    def vel_mag(self):
-        return math.hypot(*self.velocity)
+    def get_rel_speed(self, other):
+        # get the rate at which this player is approaching the other player
+        return math.cos(self.get_rel_angle(other)) * (self.velocity[0] - other.velocity[0]) + math.sin(self.get_rel_angle(other)) * (self.velocity[1] - other.velocity[1])
     
-    @vel_mag.setter
-    def vel_mag(self, value):
-        self.velocity = [vel / self.vel_mag * value for vel in self.velocity]
-    
-    @property
-    def vel_angle(self):
-        return math.atan2(*self.velocity)
-    
-    @vel_angle.setter
-    def vel_angle(self, value):
-        self.velocity = [self.vel_mag * math.cos(value), self.vel_mag * math.sin(value)]
+    def get_rel_angle(self, other):
+        # get the relative angle of this player's position to the other player's position
+        return math.atan2(self.position[1] - other.position[1], self.position[0] - other.position[0])
 
     def update(self, delta_time):
         accel = self.normalized_accel
@@ -83,6 +75,30 @@ class Player:
         # set new player velocity
         self.velocity[0] += accel[0] * self.PLAYER_ACCEL * delta_time
         self.velocity[1] += accel[1] * self.PLAYER_ACCEL * delta_time
+
+        # if player is touching another player
+        if self.hitbox.collisions:
+            for hitbox in self.hitbox.collisions:
+                # get the owner of the hitbox
+                player = next(player for player in self.PLAYERS if player.hitbox is hitbox)
+
+                # get the relative velocity of this player to the other player
+                rel_speed = self.get_rel_speed(player)
+
+                # if this player is approaching the other player
+                if rel_speed < 0:
+                    # get the relative angle of this player to the other player
+                    rel_angle = self.get_rel_angle(player)
+                    
+                    rel_speed /= 2
+
+                    # add to this player's velocity
+                    self.velocity[0] -= math.cos(rel_angle) * rel_speed
+                    self.velocity[1] -= math.sin(rel_angle) * rel_speed
+
+                    # add to the other player's velocity
+                    player.velocity[0] += math.cos(rel_angle) * rel_speed
+                    player.velocity[1] += math.sin(rel_angle) * rel_speed
         
         # limit player velocity to PLAYER_SPEED
         self.velocity[0] = max(min(self.velocity[0], self.PLAYER_SPEED), -self.PLAYER_SPEED)
@@ -102,27 +118,6 @@ class Player:
             self.velocity[0] *= -1 if self.ELASTIC_COLLISION else 0
         if self.position[1] == self.SIZE or self.position[1] == SCREEN_HEIGHT - self.SIZE:
             self.velocity[1] *= -1 if self.ELASTIC_COLLISION else 0
-        
-        # if player is touching another player, and their relative velocity is positive, make them bounce
-        if self.hitbox.collisions:
-            for other in self.hitbox.collisions:
-                # get hitbox owner
-                other = next(filter(lambda player: player.hitbox is other, self.PLAYERS))
-
-                # calculate relative velocity
-                rel_vel = [self.velocity[i] - other.velocity[i] for i in range(2)]
-
-                # if relative velocity magnitude is positive
-                if math.hypot(*rel_vel) > 0:
-                    # calculate angle of incidence
-                    angle = math.atan2(other.position[1] - self.position[1], other.position[0] - self.position[0])
-
-                    # calculate angle of reflection
-                    new_angle = 2 * angle - self.vel_angle
-
-                    # set new velocities
-                    self.vel_angle = new_angle
-                    other.vel_angle = new_angle + math.pi
 
     def draw(self, delta_time):
         color = self.color
@@ -133,6 +128,9 @@ class Player:
         
         # Draw the player's circle
         arcade.draw_circle_filled(self.position[0], self.position[1], self.SIZE, color)
+
+def polar_to_rect(radius, angle):
+    return [radius * math.cos(angle), radius * math.sin(angle)]
 
 if __name__ == "__main__":
     pass
